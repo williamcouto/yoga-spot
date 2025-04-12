b423e45e82665d8ec8b21c6ad878231f804877eb
williamcouto <willplacido40@gmail.com>
2025-04-12 18:32:07 -0300

    Feat: Inserindo uma animação da biblioteca Swiper.js nos sliders de depoimentos

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var swiper = new Swiper(".swiper", {
     slidesPerView: 2,
     spaceBetween: 70,
     loop: true,
+    effect: 'fade',
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
