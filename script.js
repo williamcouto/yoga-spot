2bfb7f3e54fb0cfb50c9b93080165b7cff0765fb
williamcouto <willplacido40@gmail.com>
2025-06-06 20:27:24 -0300

    Feat: Inserindo uma contagem dos números que representam as estatísticas da seção

--- script.js
+++ script.js
@@ -31,3 +31,25 @@ menu.addEventListener('click', () => {
     menuBg.classList.toggle('menu-bg-active')
     navItemsBar.classList.toggle('nav-open')
 })
+
+//Animação dos números
+let numStats = document.querySelectorAll('.nums')
+function updateNumStats(){
+    numStats.forEach((numsValue) => {
+        let initValue = 0
+        let finalValue = numsValue.getAttribute('data-value-stat')
+
+        let duration = Math.floor(2000 / finalValue)
+        let countStat = setInterval(function(){
+            initValue += 1
+            numsValue.textContent = initValue
+
+            if(initValue > finalValue){
+                clearInterval(countStat)
+            }
+        }, duration)
+    })
+}
+document.addEventListener('DOMContentLoaded', () => {
+    updateNumStats()
+})
